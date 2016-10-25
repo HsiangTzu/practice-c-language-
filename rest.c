07dc3e666de5a396008d5bc1117a093211a5c90a
HsiangTzu <a841225@gmail.com>
2016-10-25 13:53:57 +0800

    addLine

--- rest.c
+++ rest.c
@@ -4,14 +4,16 @@
 int main(int argc, char *argv[])
 {
   int p,sum=0,cnt=0;
+  printf("請輸入 餐點價格 或 999 離開：");
   scanf("%d", &p);
   
   while(p!=999) {
      cnt++;
      sum+=p;
+     printf("請輸入 餐點價格 或 999 離開：");
      scanf("%d",&p);
   }
-  printf("%d %d\n", cnt, sum);
+  printf("總共是 %d 道餐點， %d\n", cnt, sum);
   system("PAUSE");	
   return 0;
 }
